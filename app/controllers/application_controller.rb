732a597ae21cea78b3c2bd53ea9890c93581923a
Shane Pinnell <shane.pinnell@gmail.com>
2015-12-30 10:32:58 -0800

    Revised session to store user.id instead of user.email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user
 
+  private
   def current_user
-    @current_user ||= User.find_by(username: session[:user_id].split("@").first) if session[:user_id]
-
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
+  helper_method :current_user
 end
