18786d21b34cb86723dc01fded460d6635826090
Slobodan <slobodan.stevanovic@puhsd.org>
2016-08-24 09:41:11 -0700

    Config Deploy modified

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, '/home/deploy/apps/directory'
 
 # Default value for :linked_files is []
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
-append :linked_files, '.env'
+append :linked_files, '.env', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
