fe679a19a02daf12e24b9b0a9dcfde2208f5aaf6
Slobodan <slobodan.stevanovic@puhsd.org>
2016-10-28 22:50:09 -0700

    Group scaffold added

---
+++ db/migrate/20161028185542_add_dn_to_users.rb
@@ -0,0 +1,11 @@
+class AddDnToUsers < ActiveRecord::Migration[5.0]
+  def up
+    add_column :users, :distinguishedname, :string
+    User.import_from_ldap
+    add_index :users, :distinguishedname, :unique => true
+  end
+
+  def down
+    remove_column :users, :distinguishedname, :string
+  end
+end
